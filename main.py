f1e6e0408112ef1403b73253fef01a28b8d43897
sxq <sxq@cyx.com>
2023-03-03 23:13:28 +0800

    已对数据分析numpy以及pandas进行复习，资料代码已上传至review

--- main.py
+++ main.py
@@ -2,19 +2,18 @@
 
 # 按 Shift+F10 执行或将其替换为您的代码。
 # 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
-# 导入所需要的包
+# 导入数据分析所需要的包
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
 
+def data_cleaning():
+    print("你好")
 
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    data_cleaning()
+    print("已完成数据处理")
 
 
 
